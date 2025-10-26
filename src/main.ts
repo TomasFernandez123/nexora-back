629b21e03332c4724bd3eae6fc53bc8c645cd369
TomasFernandez123 <fernandeztomasfernandez63@gmail.com>
2025-10-25 21:54:10 -0300

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT || 3000;
+  app.enableCors()
+  await app.listen(PORT, '0.0.0.0');
 }
 bootstrap();
