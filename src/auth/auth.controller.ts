7996d60350a80eadad70778365ea79020cff16ca
Tomas <fernandeztomasfernandez63@gmail.com>
2025-11-03 19:37:27 -0300

    change: sameSite to lax

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -26,7 +26,7 @@ export class AuthController {
         const result = await this.authService.login(emailOrUsername, dto.password);
 
         if (result) {
-            res.cookie('jwt', result.token, { httpOnly: true, secure: true, maxAge: 15 * 60 * 1000, sameSite: 'none' });
+            res.cookie('jwt', result.token, { httpOnly: true, secure: true, maxAge: 15 * 60 * 1000, sameSite: 'lax' });
         }
 
         const { token, ...resultWithoutToken } = result;
