4a4dc240d0036e8918734cc2d0c00d559aeacc08
Tomas <fernandeztomasfernandez63@gmail.com>
2025-11-10 20:02:03 -0300

    fix

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,9 +3,11 @@ import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Post, PostSchema } from './schemas/post.schema';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   imports: [
+    UsersModule,
     MongooseModule.forFeature([{ name: Post.name, schema: PostSchema }])
   ],
   controllers: [PostsController],
